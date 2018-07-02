82eee57b480f07c1d3b7fed4af7797ffd963a031
ivansyvak <ivansyvak1991@gmail.com>
2018-07-02 17:39:56 +0300

    base array done

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
+import { Int4Array } from './array';
+
 async function main() {
-  console.log('ok');
+  let arr = new Int4Array(3);
+
+  arr.set(0, 1);
+  arr.set(1, 2);
+  arr.set(2, 3);
+
+  arr.forEach(console.log);
 }
 
 main();
